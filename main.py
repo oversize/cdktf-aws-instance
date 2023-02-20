7e42f189c7abd5e8c8a15de3fd1dd912b23662d5
Manuel Schmidt <manuel.schmidt@cardanofoundation.org>
2023-02-20 21:41:22 +0100

    Add cloudinit

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from constructs import Construct
 from cdktf import App, TerraformStack, TerraformOutput
 from cdktf_cdktf_provider_aws.provider import AwsProvider
-from cdktf_cdktf_provider_aws.instance import Instance
+from cdktf_cdktf_provider_aws.instance import Instance, InstanceConfig
+from cdktf_cdktf_provider_cloudinit.provider import CloudinitProvider
+from cdktf_cdktf_provider_cloudinit.data_cloudinit_config import DataCloudinitConfig, DataCloudinitConfigPart
 
 
 class MyStack(TerraformStack):
@@ -10,12 +12,30 @@ class MyStack(TerraformStack):
         super().__init__(scope, id)
 
         AwsProvider(self, "AWS", region="eu-central-1")
+        CloudinitProvider(self, "Cloudinit")
 
+        shscript = DataCloudinitConfigPart(
+            content="""#!/bin/bash
+set -x
+echo "foobar batz" > /home/ubuntu/foobar.txt
+""",
+            content_type="text/x-shellscript",
+            filename="foobar.sh"
+        )
+
+        cloudinitdata = DataCloudinitConfig(self, "DataCloudinit",
+            part=[shscript],
+            base64_encode=False,
+            gzip=False
+        )
+
+        #cfg = InstanceConfig(self, "InstanceConfig")
         instance = Instance(self, "compute",
                             ami="ami-0d1ddd83282187d18",
                             instance_type="t3.micro",
-                            subnet_id="subnet-02e07fa43d120a60d"
-                            )
+                            subnet_id="subnet-08eb6daaa70fdf686",
+                            user_data=cloudinitdata.rendered
+                        )
 
         TerraformOutput(self, "public_ip",
                         value=instance.public_ip,
